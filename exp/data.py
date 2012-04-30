b8a6f67ad3330440ece87263b2f61e36abf9f2ec
Chi Zhang <zhangchitc@gmail.com>
2012-05-01 00:37:13 +0800

    First commit

---
+++ exp/data.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# coding=utf8
+
+__author__ = "zhangchitc@gmail.com"
+
+
+class Paper:
+
+    def __init__ (self, title, abst):
+        self.title = title
+        self.abst = abst
+        self.authors = []
+
+    def add_author (self, author):
+        self.authors.append (author)
+
+    def __str__ (self):
+        ret = "Title:\n     %s\n" % self.title
+        ret += "Authors:\n"
+        for author in self.authors:
+            ret += "     %s\n" % author
+        ret += "\n\n"
+        return ret
+
+
+class Author:
+
+    def __init__ (self, name, affn):
+        self.name = name
+        self.affn = affn
+
+    def __str__ (self):
+        ret = "%s (%s)" % (self.name, self.affn)
+        return unicode (ret).encode ('utf-8')
+
+
+if __name__ == '__main__':
+    print Paper ('abc', 'def')
+    print Author ('abc', 'def')
